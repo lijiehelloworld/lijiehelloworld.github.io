c11b55684a71187437d6319e06d6ce7066bcaa4a
lijiehelloworld <lijiehelloworld@gmail.com>
2024-11-15 11:30:48 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:lijiehelloworld/lijiehelloworld.github.io.git main
+git push -f git@github.com:lijiehelloworld/lijiehelloworld.github.io.git main:gh-pages
 
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
